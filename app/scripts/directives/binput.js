00fbbbf9e7477e4e31b7e1072bed27c61dcdc08b
Eric Kizaki <erickizaki@gmail.com>
2015-09-05 13:22:56 -0500

    Added submit

--- app/scripts/directives/binput.js
+++ app/scripts/directives/binput.js
@@ -7,18 +7,54 @@
  * # bInput
  */
 angular.module('meanVoApp')
-    .directive('bInput', function() {
+    .directive('bInput', function($compile) {
+
+        var getTemplate = function(scope) {
+            var html = '';
+
+            if (scope.type === 'submit') {
+
+                if (!scope.label) {
+                    scope.label = 'Submit';
+                }
+
+                html += '<div class="form-group"><div class="col-lg-offset-2 col-lg-10"><input class="btn btn-primary" type="submit" value="{{label}}"></div></div>';
+            } else {
+                html += '<div class="form-group';
+                if (scope.required) {
+                    html += ' required';
+                }
+                html += '">';
+                html += '<label class="col-lg-2 control-label">{{label}}</label>';
+                html += '<div class="col-lg-10">';
+                html += '<input type="{{type}}" ng-model="text" class="form control"';
+                if (scope.required) {
+                    html += ' required="required" ';
+                }
+                html += '/>';
+                html += '</div></div>';
+            }
+
+            return html;
+        };
+
         return {
-            require: 'ngModel',
+            restrict: 'E',
+            require: '?ngModel',
+            replace: true,
             scope: {
                 text: '=ngModel',
-                label: '@',
-
+                label: '@?',
+                type: '@?',
+                required: '@?'
             },
-            template: '<div class="form-group">' +
-                '<label class="col-lg-2 control-label">{{label}}</label>' +
-                '<div class="col-lg-10">' + '<input type="text" ng-model="text" class="form control" />' + '</div></div>',
-            restrict: 'E',
-            link: function postLink(scope, element, attrs) {}
+            link: function postLink(scope, element, attrs) {
+                //default values
+                scope.label = scope.label || '';
+                scope.type = scope.type || 'text';
+
+                element.html(getTemplate(scope));
+                $compile(element.contents())(scope);
+            }
         };
     });
