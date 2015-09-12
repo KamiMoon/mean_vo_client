6779ea13b78e775a97abe5abb22fd5dcd483f7ee
Eric Kizaki <erickizaki@gmail.com>
2015-09-12 18:49:21 -0500

    Confirmation password must match password

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -13,20 +13,24 @@ angular.module('meanVoApp')
 
         $scope.save = function(form) {
 
-            //if (form.$valid) {
-            AuthService.createUser($scope.user)
-                .then(function() {
-                    // Account created, redirect to home
-                    //$location.path('/');
-                    ValidationService.displaySuccess('You have been registered. Check your email to verify.');
-                    $location.path('/thanks');
-                }, function(err) {
-                    ValidationService.displayErrors(form, err);
-                });
-            //} else {
-            //    console.log(form);
-            //}
+            if ($scope.user.password !== $scope.user.confirmPassword) {
+                ValidationService.error('Confirmation password must match password');
+            } else {
+                //if (form.$valid) {
+                AuthService.createUser($scope.user)
+                    .then(function() {
+                        // Account created, redirect to home
+                        //$location.path('/');
+                        ValidationService.displaySuccess('You have been registered. Check your email to verify.');
+                        $location.path('/thanks');
+                    }, function(err) {
+                        ValidationService.displayErrors(form, err);
+                    });
+                //} else {
+                //    console.log(form);
+                //}
 
+            }
 
         };
     });
